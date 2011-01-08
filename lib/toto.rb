e62ef9494573ef06411ef03f09923ac3b6c6efe1
Pat Shaughnessy <pat@patshaughnessy.net>
2011-01-08 07:52:21 -0500

    302 for aliases

--- lib/toto.rb
+++ lib/toto.rb
@@ -365,24 +365,29 @@ module Toto
       return [400, {}, []] unless @request.get?
 
       path, mime = @request.path_info.split('.')
-      route = (@config[:alias_for].call(path) || '/').split('/').reject {|i| i.empty? }
-
-      response = @site.go(route, env, *(mime ? mime : []))
-
-      @response.body = [response[:body]]
-      @response['Content-Length'] = response[:body].length.to_s unless response[:body].empty?
-      @response['Content-Type']   = Rack::Mime.mime_type(".#{response[:type]}")
-
-      # Set http cache headers
-      @response['Cache-Control'] = if Toto.env == 'production'
-        "public, max-age=#{@config[:cache]}"
+      if @config[:alias_for] && (new_path = @config[:alias_for].call(path))
+        @response.redirect(new_path)
       else
-        "no-cache, must-revalidate"
+        route = (path || '/').split('/').reject {|i| i.empty? }
+
+        response = @site.go(route, env, *(mime ? mime : []))
+
+        @response.body = [response[:body]]
+        @response['Content-Length'] = response[:body].length.to_s unless response[:body].empty?
+        @response['Content-Type']   = Rack::Mime.mime_type(".#{response[:type]}")
+
+        # Set http cache headers
+        @response['Cache-Control'] = if Toto.env == 'production'
+          "public, max-age=#{@config[:cache]}"
+        else
+          "no-cache, must-revalidate"
+        end
+
+        @response['ETag'] = %("#{Digest::SHA1.hexdigest(response[:body])}")
+
+        @response.status = response[:status]
       end
 
-      @response['ETag'] = %("#{Digest::SHA1.hexdigest(response[:body])}")
-
-      @response.status = response[:status]
       @response.finish
     end
   end
